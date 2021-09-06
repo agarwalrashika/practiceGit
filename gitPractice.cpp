fddb41bc4f7590aec5931f947779f68f85448e8b
agarwalrashika <90186777+agarwalrashika@users.noreply.github.com>
2021-09-06 23:11:08 +0530

    Update gitPractice.cpp

--- gitPractice.cpp
+++ gitPractice.cpp
@@ -3,4 +3,9 @@ using namespace std;
 int main()
 {
     cout<<"Hello, this is me practicing git\n";
+    int a=6, b=3;
+    cout<<"a+b "<<a+b<<endl;
+    cout<<"a-b "<<a-b<<endl;
+    cout<<"a*b "<<a*b<<endl;
+    cout<<"a/b "<<a/b<<endl;
 }
